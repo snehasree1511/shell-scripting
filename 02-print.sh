9550403aec19c356b43e36477f7a455f8ce56934
snehasree <snehasreeramini@gmail.com>
2022-03-13 14:59:08 +0100

    variables declaration

--- 02-print.sh
+++ 02-print.sh
@@ -2,3 +2,12 @@
 
 echo Hello World
 echo print this line
+#color codes
+#Red      31
+#Green    32
+#yello    33
+#Blue     34
+#Magenta  35
+#cyan      36
+
+#Syntax : echo -e "\e[31"
---
+++ 03-variables.sh
@@ -0,0 +1,7 @@
+#! /bin/bash
+
+a=100
+b=test
+
+echo $atimes
+echo ${b}

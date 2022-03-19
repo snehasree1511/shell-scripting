f11d013f9775f7812c1c14a58f99c9056fe61128
snehasree <snehasreeramini@gmail.com>
2022-03-19 19:51:58 +0100

    conditions file

--- 06-conditions.sh
+++ 06-conditions.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#/bin/bash
 
 #case and if are two conditional comments,if command is widely used because it has more
 #options than case command
@@ -12,8 +12,27 @@
 #commands
 #f1
 
-# shellcheck disable=SC1009
-if [1 -eq 1]
+if [1 -eq 1 ]
 then
   echo hello
+f1
+
+#expressions are importengtg
+# 1.string tests
+#operators:==,!=,-z
+#2. number tests
+#3.file tests
+
+
+a="abc"
+if["$a" == "abc"];then
+  echo both are not equal
+f1
+
+if["$a" != "abc"];then
+  echo both are not equal
+f1
+
+if[-z"$b"];then
+  echo b variable is empty
 f1

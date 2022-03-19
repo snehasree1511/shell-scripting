1921bdd6850366ffe2b3ab052732ca6fbbecf628
snehasree <snehasreeramini@gmail.com>
2022-03-19 19:37:08 +0100

    conditions file

---
+++ 05-exit-status.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+#Exit status is a number,$? is a special variable that holds this value
+#$? is going to have the exit status of last executed command.
+
+#Range of exit status is 0-255
+#0--> is global success
+#1-125--->some failure from that perticular command
+#125+-->system failures
---
+++ 06-conditions.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+#case and if are two conditional comments,if command is widely used because it has more
+#options than case command
+
+#if condition
+# if found in three forms
+
+#simple if
+# if [ expression ]
+#then
+#commands
+#f1
+
+if[1 -eq 1]
+then
+  echo hello
+f1
